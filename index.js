611e952add205b0f35433d39e436faba470bb425
rayaniqventures <ra@iqventures.vc>
2024-12-12 15:01:01 +0500

    new changes

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ const { google } = require('googleapis');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
-};
+
+app.use(cors());
+
 
 app.use(cors(corsOptions));
 
